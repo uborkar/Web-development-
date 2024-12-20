68a71631617b3debb19f0eb1d3db93d802252768
Umair Borkar <115079909+uborkar@users.noreply.github.com>
2024-12-20 18:47:14 +0530

    JavaScript Folder Added.
    This folder contains a collection of small JavaScript programs created to explore and practice various fundamental and advanced concepts of JavaScript programming. Each program demonstrates a specific feature or concept, making it a great resource for understanding and applying JavaScript in real-world scenarios.

---
+++ JavaScript/JStutorial/varletcon.js
@@ -0,0 +1,13 @@
+// function abc(){
+//     for(let i=1;i<12;i++){
+//         console.log(i);
+//     }
+//     // console.log(i);
+// }
+
+// abc();
+
+a=[1,2,3,4,5,6]
+b=[...a]
+b.pop()
+console.log(b)
---
+++ JavaScript/add.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<form name="f1">
+<h2>
+		Enter 1st Number: <input type="number" name="n1"> <br>
+		  Enter 2st Number: <input type="number" name="n2"><br><br>
+		  		
+		  		
+		 	<button onclick="add()">Add</button>
+		 	
+		 <input type="number" name="n3">
+		 </h2>
+	</form>
+	<script type="text/javascript">
+		
+			function add()
+			{
+			
+			
+			
+			}
+			
+			
+	
+	</script>
+</body>
+</html>
